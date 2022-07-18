39c4fdbd8dc34c6534e418adb445aef583150e63
Blessing <bigbadumhe@gmail.com>
2022-07-17 21:25:33 -0500

    commits from build

---
+++ client/build/precache-manifest.bd22edbc27a48a5f1fce42dac56871b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b882c7eecf0b55fc742360cdfb32e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d95d758d6375fb6b65",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "5ae2498fb2f90b7b9e15",
+    "url": "/static/js/2.f7661d55.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f7661d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d95d758d6375fb6b65",
+    "url": "/static/js/main.35c9bac6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.35c9bac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
